a499bf63836c56ddd217b3b24f3aefa56fe637bc
Keisuke MORI <keisuke.mori+ha@gmail.com>
2019-08-07 13:16:37 +0900

    demo-scenario: vm-nw-connect.sh: allow to run when VM is off

--- demo-scenario/vm-nw-connect.sh
+++ demo-scenario/vm-nw-connect.sh
@@ -3,9 +3,23 @@
 BASEDIR=$(dirname "$0")
 . $BASEDIR/common.sh
 
-msg "reconnect worker1"
-run_cmd sh -c "
- VBoxManage controlvm worker1 setlinkstate1 on ;
- VBoxManage controlvm worker1 setlinkstate2 on"
+if ! which VBoxManage >/dev/null; then
+    echo "This script must be run on the host node."
+    exit 1
+fi
 
+if VBoxManage list runningvms | grep "worker1" >/dev/null; then
+    # VM is running state
+    msg "reconnect worker1"
+    run_cmd sh -c "
+     VBoxManage controlvm worker1 setlinkstate1 on ;
+     VBoxManage controlvm worker1 setlinkstate2 on"
 
+else
+    # VM is not running state
+    msg "reconnect worker1"
+    run_cmd sh -c "
+     VBoxManage modifyvm worker1 --cableconnected1 on ;
+     VBoxManage modifyvm worker1 --cableconnected2 on"
+
+fi
--- demo-scenario/vm-nw-disconnect.sh
+++ demo-scenario/vm-nw-disconnect.sh
@@ -3,7 +3,23 @@
 BASEDIR=$(dirname "$0")
 . $BASEDIR/common.sh
 
-msg "disconnect worker1"
-run_cmd sh -c "
- VBoxManage controlvm worker1 setlinkstate1 off ;
- VBoxManage controlvm worker1 setlinkstate2 off"
+if ! which VBoxManage >/dev/null; then
+    echo "This script must be run on the host node."
+    exit 1
+fi
+
+if VBoxManage list runningvms | grep "worker1" >/dev/null; then
+    # VM is running state
+    msg "disconnect worker1"
+    run_cmd sh -c "
+     VBoxManage controlvm worker1 setlinkstate1 off ;
+     VBoxManage controlvm worker1 setlinkstate2 off"
+
+else
+    # VM is not running state
+    msg "disconnect worker1"
+    run_cmd sh -c "
+     VBoxManage modifyvm worker1 --cableconnected1 off ;
+     VBoxManage modifyvm worker1 --cableconnected2 off"
+
+fi
